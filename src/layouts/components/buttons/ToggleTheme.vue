7937418427b00a2d9a743e28f83bd4d26cac1c1d
xiafan <805091667@qq.com>
2025-01-17 09:08:43 +0800

    完善主题切换

--- src/layouts/components/buttons/ToggleTheme.vue
+++ src/layouts/components/buttons/ToggleTheme.vue
@@ -1,37 +1,63 @@
 <template>
-  <NButton 
-    circle
-    @click="toggleDark()" 
-  >
+  <NButton circle @click="toggleDark">
     <template #icon>
-      <template v-if="!isDark">
-        <NIcon>
+      <NIcon>
+        <template v-if="!isDark">
           <SunIcon />
-        </NIcon>
-      </template>
-      <template v-else>
-        <NIcon>
+        </template>
+        <template v-else>
           <MoonIcon />
-        </NIcon>
-      </template>
+        </template>
+      </NIcon>
     </template>
   </NButton>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, nextTick } from 'vue'
 import { useDark, useToggle } from '@vueuse/core';
 import { NButton, NIcon } from 'naive-ui';
-import { 
-  WbSunnyFilled as SunIcon, 
-  NightlightOutlined as MoonIcon 
+import {
+  WbSunnyFilled as SunIcon,
+  NightlightOutlined as MoonIcon
 } from '@vicons/material';
 
 const isDark = useDark();
 
-const toggleDark = async () => {
-  console.log("toggle dark " + isDark.value + " => " +!isDark.value)
-  useToggle(isDark)()
+const toggleDark = async (e) => {
+
+  const handler = async () => {
+    console.log("toggle dark " + isDark.value + " => " + !isDark.value)
+    useToggle(isDark)()
+    await nextTick()
+  }
+
+  if (!document.startViewTransition) {
+    return handler()
+  }
+
+  const x = e.clientX
+  const y = e.clientY
+
+  const clipPath = [
+    `circle(0px at ${x}px ${y}px)`,
+    `circle(${Math.hypot(
+      Math.max(x, window.innerWidth - x),
+      Math.max(y, window.innerHeight - y),
+    )}px at ${x}px ${y}px)`,
+  ]
+
+  await document.startViewTransition(handler).ready
+
+  // 相关CSS在 @/index.css 中
+  document.documentElement.animate(
+    { clipPath: isDark.value ? clipPath.reverse() : clipPath },
+    {
+      duration: 300,
+      easing: 'ease-in',
+      pseudoElement: `::view-transition-${isDark.value ? 'old' : 'new'}(root)`,
+    },
+  )
 }
 
 </script>
